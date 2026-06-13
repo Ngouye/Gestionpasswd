2661e0a82adc9e33c507afc5f002908f3e6a7992
ngouye <gningngouye456@gmail.com>
2026-06-13 14:43:23 +0000

    Fix: configure dynamic authentication tokens for kubernetes and helm providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,3 +158,19 @@ resource "helm_release" "aws_load_balancer_controller" {
     value = module.vpc.vpc_id
   }
 }
+# ==========================================================
+# CONFIGURATION DES PROVIDERS POUR S'AUTHENTIFIER SUR EKS
+# ==========================================================
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
