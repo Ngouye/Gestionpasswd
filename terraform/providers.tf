84a3d0d6a4b2c7d3df47139812ce98f8f2fb4747
ngouye <gningngouye456@gmail.com>
2026-06-13 02:41:20 +0000

    Fix: add empty backend s3 block to providers and cleanup syntax

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  # AJOUT CRUCIAL : Dit à Terraform d'attendre une configuration de backend S3 externe
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -21,11 +24,11 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Correction mineure de syntaxe sur l'appel du certificat (évite les avertissements de dépréciation)
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
 }
 
 provider "helm" {
@@ -33,6 +36,5 @@ provider "helm" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
   }
 }
